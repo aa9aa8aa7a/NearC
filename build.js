d17b3f753acf4530c62d12dc65ffd74da16addd1
NearC <aa9aa8aa7a@163.com>
2017-07-04 16:56:28 +0800

    update

--- build.js
+++ build.js
@@ -2,7 +2,7 @@ const fs = require('fs') ;
 const path = require('path');
 fs.readdir('./markdown',function(err,files){
     for(var i=0;i<files.length;i++){
-    var ph = path.join('./markdown',files[0]);
+    var ph = path.join('./markdown',files[i]);
     var markdown = fs.readFileSync(ph).toString();
     var temp = fs.readFileSync('./temp.html').toString();
     var result = temp.replace('%t%',markdown);
